4ac0d480d674f69720f294806406b2aaa512c599
Rithik Bhandari <rithikbhandari895@gmail.com>
2020-10-18 21:41:37 +0530

    - changed app font - added device orientation constraints

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:password_manager/constants.dart';
 import 'package:password_manager/models/provider_class.dart';
 import 'package:password_manager/models/route_generator.dart';
 import 'package:password_manager/screens/initial_screen_handler.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  final ThemeData appTheme = ThemeData(
+    fontFamily: 'ProductSans',
+    brightness: Brightness.dark,
+    appBarTheme: AppBarTheme(color: kSecondaryColor),
+    scaffoldBackgroundColor: kScaffoldBackgroundColor,
+    dialogBackgroundColor: kCardBackgroundColor,
+    canvasColor: kCardBackgroundColor,
+    snackBarTheme: SnackBarThemeData(
+      backgroundColor: Colors.deepPurple,
+      contentTextStyle: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<ProviderClass>(
       create: (context) => ProviderClass(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark().copyWith(
-          appBarTheme: AppBarTheme(color: kSecondaryColor),
-          scaffoldBackgroundColor: kScaffoldBackgroundColor,
-          dialogBackgroundColor: kCardBackgroundColor,
-          canvasColor: kCardBackgroundColor,
-          snackBarTheme: SnackBarThemeData(
-            backgroundColor: Colors.deepPurple,
-            contentTextStyle: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ),
+        theme: this.appTheme,
+        darkTheme: this.appTheme,
         onGenerateRoute: RouteGenerator.generateRoute,
         initialRoute: InitialScreenHandler.id,
       ),
